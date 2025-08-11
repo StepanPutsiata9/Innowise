af10d8e124122b0051f973e3d99335298ce55d18
Stepan Putsiata <sstepanovich546@gmail.com>
2025-08-11 20:56:05 +0300

    before new branch

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,20 +4,20 @@ import { Tabs } from 'expo-router';
 import { useSelector } from 'react-redux';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props ) {
+function TabBarIcon(props) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-  const theme=useSelector((state)=>state.theme.mode);
+  const theme = useSelector((state) => state.theme.mode);
   return (
     <Tabs
       screenOptions={{
-         tabBarActiveTintColor: theme === "dark" ? "#fff" : "#000",
+        tabBarActiveTintColor: theme === "dark" ? "#fff" : "#000",
         tabBarInactiveTintColor: theme === "dark" ? "#888" : "#666",
-        headerShown:false,
+        headerShown: false,
         tabBarStyle: {
-          height:75,
+          height: 75,
           backgroundColor: theme === "dark" ? "#000" : "#fff",
           borderTopColor: theme === "dark" ? "#333" : "#ddd",
         },
@@ -26,15 +26,15 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Main',
-          headerShown:false,
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        
+
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          headerShown:false,
+          headerShown: false,
           title: 'Settings',
           tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
         }}
